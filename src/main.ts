82b0723f910dcb4d786e822901c15b0fba2f951c
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2023-09-09 14:44:55 +0700

    Update swagger ui route

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors();
 
   app.setGlobalPrefix('api', {
-    exclude: [''],
+    exclude: ['', '/swagger'],
   });
 
   const config = new DocumentBuilder()
@@ -17,7 +17,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
